4201eaf472c300eab19910bf2dbb39588eb42274
imad <imadatyatalah@gmail.com>
2021-02-01 19:38:23 +0100

    Remove Heading tag

--- components/mediaComponents/mediaCardInfo.jsx
+++ components/mediaComponents/mediaCardInfo.jsx
@@ -1,4 +1,4 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, chakra } from "@chakra-ui/react";
 import NextLink from "next/link";
 
 import Date from "../shared/date";
@@ -7,12 +7,11 @@ const CardInfo = ({ href, title, date }) => (
   <>
     <Box px="1rem" pb="0.5rem">
       <NextLink href={href}>
-        <a>
-          <Heading as="h1" fontSize="1rem" fontWeight="500" pb="0.25rem">
-            {title}
-          </Heading>
-        </a>
+        <chakra.a href={href} fontSize="1rem" fontWeight="500" pb="0.25rem">
+          {title}
+        </chakra.a>
       </NextLink>
+      <br />
       {date && (
         <Box as={Date} color="gray.600" fontSize="sm" datestring={date} />
       )}
