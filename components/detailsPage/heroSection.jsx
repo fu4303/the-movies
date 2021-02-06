4f532389224fb953dd2b1df280be002895c42ff8
imad <imadatyatalah@gmail.com>
2021-02-06 19:22:31 +0100

    edit details page's hero section

--- components/detailsPage/heroSection.jsx
+++ components/detailsPage/heroSection.jsx
@@ -1,115 +1,123 @@
 import NextImage from "next/image";
 
 import { IMAGE_BASE_URL, POSTER_SIZE, PROFILE_SIZE } from "../../config";
-import { MediaInfoCard } from "./infoCard";
-import { PersonInfoCard } from "./infoCard";
+import { MediaInfoCard, PersonInfoCard } from "./infoCard";
 import { Box } from "@chakra-ui/react";
 import Container from "./container";
 import Backdrop from "./backdrop";
 
 import styles from "./styles/heroSection.module.css";
 
-export const MediaHeroSection = (props) => (
-  <>
-    <Backdrop backdropPath={props.backdrop} isMinHeightScreenFull>
-      <Container backBtnHref={props.backBtnHref}>
-        {props.poster ? (
-          <Box
-            className={styles.mediaImageContainer}
-            alignSelf="center"
-            display="flex"
-          >
-            <NextImage
-              src={`${IMAGE_BASE_URL}${POSTER_SIZE}${props.poster}`}
-              alt={props.title}
-              title={props.title}
-              width="342"
-              height="513"
-              priority
-              layout="fixed"
-              className={styles.mediaImage}
-            />
-          </Box>
-        ) : (
-          <Box
-            className={styles.mediaImageContainer}
-            alignSelf="center"
-            bgColor="tailwindGray.400"
-          >
-            <NextImage
-              src="/image.svg"
-              title={props.title}
-              alt="image-icon ionicons ionicons-icon"
-              width="342"
-              height="513"
-              priority
-              layout="fixed"
-              className={styles.fallBackMediaImage}
-            />
-          </Box>
-        )}
+export const MediaHeroSection = (props) => {
+  const {
+    backdrop,
+    backBtnHref,
+    poster,
+    title,
+    mediaType,
+    pId,
+    mediaId,
+  } = props;
 
-        <MediaInfoCard
-          href={`/${props.mediaType}/${props.pId}/${props.mediaId}`}
-          {...props}
-        />
-      </Container>
-    </Backdrop>
-  </>
-);
+  return (
+    <>
+      <Backdrop backdropPath={backdrop} isMinHeightScreenFull>
+        <Container backBtnHref={backBtnHref}>
+          {poster ? (
+            <Box
+              className={styles.mediaImageContainer}
+              alignSelf="center"
+              display="flex"
+            >
+              <NextImage
+                src={`${IMAGE_BASE_URL}${POSTER_SIZE}${poster}`}
+                alt={title}
+                title={title}
+                width="342"
+                height="513"
+                priority
+                layout="fixed"
+                className={styles.mediaImage}
+              />
+            </Box>
+          ) : (
+            <Box
+              className={styles.mediaImageContainer}
+              alignSelf="center"
+              bgColor="tailwindGray.400"
+            >
+              <NextImage
+                src="/image.svg"
+                title={title}
+                alt="image-icon ionicons ionicons-icon"
+                width="342"
+                height="513"
+                priority
+                layout="fixed"
+                className={styles.fallBackMediaImage}
+              />
+            </Box>
+          )}
 
-export const PersonHeroSection = (props) => (
-  <>
-    <Backdrop
-      backdropPath={
-        props.data.cast.sort((a, b) => b.popularity - a.popularity)[0]
-          .backdrop_path
-      }
-      isMinHeightScreenFull
-    >
-      <Container backBtnHref={props.backBtnHref}>
-        {props.profile ? (
-          <Box
-            className={styles.mediaImageContainer}
-            alignSelf="center"
-            display="flex"
-          >
-            <NextImage
-              src={`${IMAGE_BASE_URL}${PROFILE_SIZE}${props.profile}`}
-              alt={props.name}
-              title={props.name}
-              width="300"
-              height="450"
-              priority
-              layout="fixed"
-              className={styles.mediaImage}
-            />
-          </Box>
-        ) : (
-          <Box
-            className={styles.mediaImageContainer}
-            alignSelf="center"
-            display="flex"
-            bgColor="tailwindGray.400"
-          >
-            <NextImage
-              src="/person-sharp.svg"
-              title={props.name}
-              alt="person-sharp-icon ionicons ionicons-icon"
-              width="300"
-              height="450"
-              priority
-              layout="fixed"
-              className={styles.fallBackMediaImage}
-            />
-          </Box>
-        )}
+          <MediaInfoCard href={`/${mediaType}/${pId}/${mediaId}`} {...props} />
+        </Container>
+      </Backdrop>
+    </>
+  );
+};
 
-        <PersonInfoCard
-          href={`/person/${props.pId}/${props.personId}`}
-          {...props}
-        />
-      </Container>
-    </Backdrop>
-  </>
-);
+export const PersonHeroSection = (props) => {
+  const { data, backBtnHref, profile, name, pId, personId } = props;
+
+  return (
+    <>
+      <Backdrop
+        backdropPath={
+          data.cast.sort((a, b) => b.popularity - a.popularity)[0].backdrop_path
+        }
+        isMinHeightScreenFull
+      >
+        <Container backBtnHref={backBtnHref}>
+          {profile ? (
+            <Box
+              className={styles.mediaImageContainer}
+              alignSelf="center"
+              display="flex"
+            >
+              <NextImage
+                src={`${IMAGE_BASE_URL}${PROFILE_SIZE}${profile}`}
+                alt={name}
+                title={name}
+                width="300"
+                height="450"
+                priority
+                layout="fixed"
+                className={styles.mediaImage}
+              />
+            </Box>
+          ) : (
+            <Box
+              className={styles.mediaImageContainer}
+              alignSelf="center"
+              display="flex"
+              bgColor="tailwindGray.400"
+            >
+              <NextImage
+                src="/person-sharp.svg"
+                title={name}
+                alt="person-sharp-icon ionicons ionicons-icon"
+                width="300"
+                height="450"
+                priority
+                layout="fixed"
+                className={styles.fallBackMediaImage}
+              />
+            </Box>
+          )}
+
+          <PersonInfoCard href={`/person/${pId}/${personId}`} {...props} />
+        </Container>
+      </Backdrop>
+    </>
+  );
+};
