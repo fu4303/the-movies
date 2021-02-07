be14815791f76c8da2953c7d47c5e6f91fad877b
imad <imadatyatalah@gmail.com>
2021-02-07 16:40:12 +0100

    test getServerSideProps with swr performance

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,53 @@
-import config from "../config";
+import config, { API_KEY, API_URL, fetcher } from "../config";
 import SEO from "../components/shared/seo";
+import useSWR from "swr";
+import dynamic from "next/dynamic";
+import { pageTitle } from "../components/helpers/pageTitle";
 
-const Home = () => {
+const MovieCard = dynamic(() =>
+  import("../components/mediaComponents/mediaCards").then(
+    (mod) => mod.MediaCards
+  )
+);
+const Container = dynamic(() => import("../components/shared/cardContainer"));
+
+const Home = ({ movies }) => {
+  const {
+    data,
+  } = useSWR(
+    `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
+    fetcher,
+    { initialData: movies }
+  );
+
   return (
     <>
-      <SEO title="Home" description={config.description} />
+      <SEO
+        title={`${pageTitle("popular")} Movies`}
+        description={config.description}
+      />
+      <Container title={`${pageTitle("popualr")} Movies`}>
+        <MovieCard
+          data={data.results}
+          mediaType="movie"
+          pId="popular"
+          isMovie
+        />
+      </Container>
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const movies = await fetcher(
+    `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+  );
+
+  return {
+    props: {
+      movies,
+    },
+  };
 };
 
 export default Home;
