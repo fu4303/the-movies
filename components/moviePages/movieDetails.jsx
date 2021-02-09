73a34dafc260e6150efc33fa448a1e9878353adc
imad <imadatyatalah@gmail.com>
2021-02-09 20:28:52 +0100

    make the code more readable

--- components/moviePages/movieDetails.jsx
+++ components/moviePages/movieDetails.jsx
@@ -3,18 +3,22 @@ import ActorsCards from "../detailsPage/actorsCards";
 import Trailer from "../detailsPage/trailer";
 import Recommendations from "../detailsPage/recommendations";
 
-const MovieDetails = (props) => (
-  <>
-    <MediaHeroSection mediaType="movie" mediaId={props.movieId} {...props} />
-    <ActorsCards
-      mediaType="movie"
-      title="Top Billed Cast"
-      mediaId={props.movieId}
-      pId={props.pId}
-    />
-    <Trailer mediaType="movie" mediaId={props.movieId} />
-    <Recommendations mediaType="movie" mediaId={props.movieId} isMovie />
-  </>
-);
+const MovieDetails = (props) => {
+  const { movieId, pId } = props;
+
+  return (
+    <>
+      <MediaHeroSection mediaType="movie" mediaId={movieId} {...props} />
+      <ActorsCards
+        mediaType="movie"
+        title="Top Billed Cast"
+        mediaId={movieId}
+        pId={pId}
+      />
+      <Trailer mediaType="movie" mediaId={movieId} pId={pId} />
+      <Recommendations mediaType="movie" mediaId={movieId} pId={pId} isMovie />
+    </>
+  );
+};
 
 export default MovieDetails;
--- components/tvPages/tvShowDetails.jsx
+++ components/tvPages/tvShowDetails.jsx
@@ -3,18 +3,22 @@ import ActorsCards from "../detailsPage/actorsCards";
 import Trailer from "../detailsPage/trailer";
 import Recommendations from "../detailsPage/recommendations";
 
-const TVShowDetails = (props) => (
-  <>
-    <MediaHeroSection mediaType="tv" mediaId={props.tvShowId} {...props} />
-    <ActorsCards
-      mediaType="tv"
-      title="Series Cast"
-      mediaId={props.tvShowId}
-      pId={props.pId}
-    />
-    <Trailer mediaType="tv" mediaId={props.tvShowId} />
-    <Recommendations mediaType="tv" mediaId={props.tvShowId} />
-  </>
-);
+const TVShowDetails = (props) => {
+  const { tvShowId, pId } = props;
+
+  return (
+    <>
+      <MediaHeroSection mediaType="tv" mediaId={tvShowId} {...props} />
+      <ActorsCards
+        mediaType="tv"
+        title="Series Cast"
+        mediaId={tvShowId}
+        pId={pId}
+      />
+      <Trailer mediaType="tv" mediaId={tvShowId} pId={pId} />
+      <Recommendations mediaType="tv" mediaId={tvShowId} pId={pId} />
+    </>
+  );
+};
 
 export default TVShowDetails;
