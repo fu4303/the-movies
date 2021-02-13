e125f5b7966339db0c586d1988737e5a0c3b2f64
imad <imadatyatalah@gmail.com>
2021-02-13 13:54:08 +0100

    fetch movies

---
+++ lib/movies.jsx
@@ -0,0 +1,9 @@
+import { fetcher, API_URL, API_KEY } from "../config";
+
+export const getMovies = async (pId, page) => {
+  const movies = await fetcher(
+    `${API_URL}movie/${pId}?api_key=${API_KEY}&language=en-US&page=${page}`
+  );
+
+  return movies;
+};
