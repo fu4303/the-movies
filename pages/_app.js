d1ed43c75ca8ed717fbc4be822d1292a3f688c9d
imad <imadatyatalah@gmail.com>
2021-03-03 18:40:45 +0100

    Fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,44 +1,12 @@
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
+import { theme } from "../theme";
 import Layout from "../components/layout";
 
-import "@fontsource/poppins/300.css";
-import "@fontsource/poppins/400.css";
-import "@fontsource/poppins/500.css";
-import "@fontsource/poppins/600.css";
-
-const theme = extendTheme({
-  colors: {
-    tailwindCyan: {
-      50: "#ECFEFF",
-      100: "#CFFAFE",
-      200: "#A5F3FC",
-      300: "#67E8F9",
-      400: "#22D3EE",
-      500: "#06B6D4",
-      600: "#0891B2",
-      700: "#0E7490",
-      800: "#155E75",
-      900: "#164E63",
-    },
-    tailwindGray: {
-      50: "#FAFAFA",
-      100: "#F4F4F5",
-      200: "#E4E4E7",
-      300: "#D4D4D8",
-      400: "#A1A1AA",
-      500: "#71717A",
-      600: "#52525B",
-      700: "#3F3F46",
-      800: "#27272A",
-      800: "#18181B",
-    },
-  },
-  fonts: {
-    heading: "Poppins, -apple-system",
-    body: "Poppins, -apple-system",
-  },
-});
+import "@fontsource/poppins/latin-300.css";
+import "@fontsource/poppins/latin-400.css";
+import "@fontsource/poppins/latin-500.css";
+import "@fontsource/poppins/latin-600.css";
 
 const MyApp = ({ Component, pageProps }) => (
   <>
