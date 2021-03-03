27cb0e7df47669acabefc5173a9bf5098d0d0bb5
imad <imadatyatalah@gmail.com>
2021-03-03 18:41:39 +0100

    Chakra UI theme

---
+++ theme/index.jsx
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+
+import { fonts } from "./fonts";
+import { colors } from "./colors";
+
+export const theme = extendTheme({
+  fonts,
+  colors,
+});
