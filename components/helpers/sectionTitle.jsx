921aeafc94dfe36f1b1a2aae61ac8985b50acb7a
imad <imadatyatalah@gmail.com>
2021-03-05 14:15:54 +0100

    Fix

--- components/helpers/sectionTitle.jsx
+++ components/helpers/sectionTitle.jsx
@@ -1,18 +1,21 @@
-import { Box, chakra } from "@chakra-ui/react";
+import { chakra, Heading } from "@chakra-ui/react";
 import NextLink from "next/link";
 
-export const SectionTitle = (props) => (
-  <>
-    <Box {...props}>
-      <NextLink href={props.href || "/"}>
-        <chakra.a
-          href={props.href || "/"}
-          fontSize={{ base: "1.5rem", lg: "1.75rem" }}
-          fontWeight="600"
-        >
-          {props.children}
-        </chakra.a>
-      </NextLink>
-    </Box>
-  </>
-);
+export const SectionTitle = (props) => {
+  const { href, children } = props;
+
+  return (
+    <>
+      <Heading as="h1" {...props}>
+        <NextLink href={href || "/"}>
+          <chakra.a
+            href={href || "/"}
+            fontSize={{ base: "1.5rem", lg: "1.75rem" }}
+          >
+            {children}
+          </chakra.a>
+        </NextLink>
+      </Heading>
+    </>
+  );
+};
