3282594e2fec5c2bf280d6cb85e3ab498704a99f
imad <imadatyatalah@gmail.com>
2021-03-10 12:15:16 +0100

    Added default seo config

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,8 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { DefaultSeo } from "next-seo";
 
 import { theme } from "../theme";
+import SEO from "../next-seo.config";
 import Layout from "../components/layout";
 
 import "@fontsource/poppins/latin-300.css";
@@ -11,6 +13,8 @@ import "../scss/index.scss";
 
 const MyApp = ({ Component, pageProps }) => (
   <>
+    <DefaultSeo {...SEO} />
+
     <ChakraProvider theme={theme}>
       <Layout>
         <Component {...pageProps} />
